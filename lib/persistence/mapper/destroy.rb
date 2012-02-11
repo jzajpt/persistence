6a0d14a27cdce65d3869ca297c7e4297262d6605
Jiří Zajpt <jzajpt@blueberry.cz>
2012-02-11 12:49:38 +0100

    Mapper.destroy can be called with object or ID

--- lib/persistence/mapper/destroy.rb
+++ lib/persistence/mapper/destroy.rb
@@ -6,11 +6,26 @@ module Persistence
       # Destroys resource with given ID.
       #
       # @param [BSON::ObjectID, string] id Resource ID
-      def destroy(id)
-        object = self.find id
-        object && run_observers(:destroy, object) do
-          self.collection_adapter.destroy_resource(id)
+      def destroy(id_or_object)
+        if object_with_id?(id_or_object)
+          object = id_or_object
+        else
+          object = self.find id_or_object
+          return unless object
         end
+        destroy_object object
+      end
+
+      protected
+
+      def destroy_object(object)
+        run_observers(:destroy, object) do
+          self.collection_adapter.destroy_resource(object.id)
+        end
+      end
+
+      def object_with_id?(object)
+        object.respond_to?(:id) && object.id.present?
       end
     end
   end
